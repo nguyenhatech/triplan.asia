ecd5c2619ad7baf89c2938b587cc7c3cb3b1e4eb
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-09 17:24:19 +0700

    :zap: Langding

--- resources/views/web/homes/index.blade.php
+++ resources/views/web/homes/index.blade.php
@@ -11,19 +11,46 @@
             height: 100vh;
             width: 100vw;
             background-size: cover;
+        }
+        #slideshow {
+            position: relative;
+            width: 100vw;
+            height: 100vh;
+        }
 
+        #slideshow > div {
+            position: absolute;
+            width: 100vw;
+            height: 100vh;
         }
     </style>
 @endsection
 
 @section('content')
-    <div class="web-home">
-        <div class="image">
-
-        </div>
+    <div id="slideshow">
+       <div>
+         <img src="{{ get_asset('web/images/banners/baner1.jpg') }}">
+       </div>
+       <div>
+         <img src="{{ get_asset('web/images/banners/baner2.jpg') }}">
+       </div>
+       <div>
+         Pretty cool eh? This slide is proof the content can be anything.
+       </div>
     </div>
 @endsection
 
 @section('scripts')
+    <script type="text/javascript">
+        $("#slideshow > div:gt(0)").hide();
 
+        setInterval(function() {
+          $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+        },  3000);
+    </script>
 @endsection

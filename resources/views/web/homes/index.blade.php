46af20a07d28e840cd87475b42b6fd1e637a7d67
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-09 17:40:09 +0700

    :zap: Langding

--- resources/views/web/homes/index.blade.php
+++ resources/views/web/homes/index.blade.php
@@ -6,57 +6,73 @@
 
 @section('styles')
     <style type="text/css">
-        .image {
-            background-image: url({{ get_asset('web/images/banners/baner1.jpg') }});
-            height: 100vh;
-            width: 100vw;
-            background-size: cover;
-            background-position: center center;
+        .web-home {
+            position: relative;
         }
-        #slideshow {
+        .web-home .slideshow {
             position: relative;
             width: 100vw;
             height: 100vh;
         }
 
-        #slideshow > div {
+        .web-home .slideshow > div {
             position: absolute;
             width: 100vw;
             height: 100vh;
         }
-        #slideshow img {
+        .web-home .slideshow img {
             width: 100%;
             height: 100%;
             object-fit: cover;
+        }
+        .web-home .search {
+            position: absolute;
+            z-index: 1200;
+            top: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100vw;
+            height: 100vh;
+        }
+        .web-home .search div {
+            padding: 15px 20px;
+            background-color: #ffffff96;
+            border-radius: 4px;
         }
     </style>
 @endsection
 
 @section('content')
-    <div id="slideshow">
-       <div>
-         <img src="{{ get_asset('web/images/banners/baner1.jpg') }}">
-       </div>
-       <div>
-         <img src="{{ get_asset('web/images/banners/baner2.jpg') }}">
-       </div>
-       <div>
-         Pretty cool eh? This slide is proof the content can be anything.
-       </div>
+    <div class="web-home">
+        <div class="slideshow">
+           <div>
+             <img src="{{ get_asset('web/images/banners/baner1.jpg') }}">
+           </div>
+           <div>
+             <img src="{{ get_asset('web/images/banners/baner2.jpg') }}">
+           </div>
+        </div>
+        <div class="search">
+            <div>
+                <h1>Bạn muốn đi đâu ?</h1>
+                <h3>Chuyến đi, trải nghiệm và địa điểm. Tất cả trong một dịch vụ</h3>
+            </div>
+        </div>
     </div>
 @endsection
 
 @section('scripts')
     <script type="text/javascript">
-        $("#slideshow > div:gt(0)").hide();
+        $(".slideshow > div:gt(0)").hide();
 
         setInterval(function() {
-          $('#slideshow > div:first')
+          $('.slideshow > div:first')
             .fadeOut(1000)
             .next()
             .fadeIn(1000)
             .end()
             .appendTo('#slideshow');
-        },  3000);
+        },  3500);
     </script>
 @endsection

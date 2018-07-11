44775c642c557b2d5a3e0893bd0b98aecda8c540
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-11 13:40:08 +0700

    :zap: Home

--- public/web/librarys/customs-1.0.1/js/web.js
+++ public/web/librarys/customs-1.0.1/js/web.js
@@ -1,15 +1,12 @@
 jQuery(document).ready(function($) {
-    $("#backToTop").click(function () {
-       $("html, body").animate({scrollTop: 0}, 400);
-    });
-
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('.header-top-navigation').addClass('header-top-navigation-scroll');
-            $('.header-top-navigation .nav-link').css('color', '#000 !important');
+            $('.navbar-light .navbar-nav .nav-link').addClass('nav-link-scroll');
             $('.header-top-navigation .navbar-brand span').css('color', '#000');
         } else {
             $('.header-top-navigation').removeClass('header-top-navigation-scroll');
+            $('.navbar-light .navbar-nav .nav-link').removeClass('nav-link-scroll');
             $('.header-top-navigation .navbar-brand span').css('color', '#fff');
         }
     });

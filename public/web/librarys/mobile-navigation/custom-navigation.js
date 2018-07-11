a24dc02b3aee9f16683c9a24af976d7d3b4d63bf
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-11 14:31:51 +0700

    :zap: Home

--- public/web/librarys/mobile-navigation/custom-navigation.js
+++ public/web/librarys/mobile-navigation/custom-navigation.js
@@ -3,19 +3,21 @@ jQuery(document).ready(function($) {
 
     $('#td-top-mobile-toggle, .td-mobile-close a').click(function(e) {
         e.preventDefault();
-        document.ontouchmove = function(event){
-            event.preventDefault();
-        }
         jQuery("body").hasClass("td-menu-mob-open-menu") ? jQuery("body").removeClass("td-menu-mob-open-menu") : jQuery("body").addClass("td-menu-mob-open-menu")
         jQuery("body").hasClass("td-menu-mob-open-menu") ? jQuery("body").addClass("overflow-hidden") : jQuery("body").removeClass("overflow-hidden")
     });
 
     $('#td-header-search-button-mob, .td-search-close a').click(function(e) {
         e.preventDefault();
-        document.ontouchmove = function(event){
-            event.preventDefault();
+        if (jQuery("body").hasClass("td-search-opened")) {
+            document.ontouchmove = function(e){ return true; }
+            jQuery("body").removeClass("td-search-opened")
+        } else {
+            document.ontouchmove = function(event){
+                event.preventDefault();
+            }
+            jQuery("body").addClass("td-search-opened")
         }
-        jQuery("body").hasClass("td-search-opened") ? jQuery("body").removeClass("td-search-opened") : jQuery("body").addClass("td-search-opened")
         jQuery("body").hasClass("td-search-opened") ? jQuery("body").addClass("overflow-hidden") : jQuery("body").removeClass("overflow-hidden")
     });
 });

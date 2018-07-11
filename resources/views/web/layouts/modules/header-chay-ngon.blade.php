3db0038cbfb9f28a4a8f411573803645b0477a0b
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-11 10:49:42 +0700

    :zap: Home

--- resources/views/web/layouts/modules/header-chay-ngon.blade.php
+++ resources/views/web/layouts/modules/header-chay-ngon.blade.php
@@ -4,10 +4,10 @@
             <a class="navbar-brand d-flex align-items-center" href="{{ env('APP_URL') }}">
                 <span>Triplan</span>
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" id="td-header-search-button-mob" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="#">Trang chủ</a>

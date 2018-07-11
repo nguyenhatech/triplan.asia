8b0f39c1747306b4ddb1f825ed317e6a87b23e3f
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-11 09:24:59 +0700

    :zap: Home

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -3,22 +3,13 @@
     <head>
         @include('web/layouts/modules/metadata')
         <link rel="stylesheet" type="text/css" href="{{ get_asset('web/librarys/bootstrap-4.1.1/dist/css/bootstrap.min.css') }}">
-        {{-- <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700" rel="stylesheet"> --}}
-        {{-- <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet"> --}}
         <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
         <link rel="stylesheet" type="text/css" href="{{ get_asset('web/librarys/customs-1.0.1/css/style.css') }}">
         @yield('styles')
     </head>
     <body>
-        <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
+        @include('web.layouts.modules.facebook')
         <header>
             @include('web.layouts.modules.header')
         </header>

70473103781f2d240889db459e9ea4035b92f4cb
Kien NV <vankien9x@gmail.com>
2018-07-12 15:23:08 +0700

    edit get image func

--- app/Repositories/Places/Place.php
+++ app/Repositories/Places/Place.php
@@ -14,11 +14,15 @@ class Place extends Entity
      */
     public $fillable = [];
 
-    public function getImage($thumbnail = false)
+    public function getImage($type = null)
     {
-        if ($this->image == '') return get_asset('images/places/place_default.png');
-
-        return env('IMAGE_PATH') . $this->image;
+        if ($type == 'sm') {
+            return env('IMAGE_PATH') . '350x200/' . $this->image;
+        } else if ($type == 'md') {
+            return env('IMAGE_PATH') . '750x390/' . $this->image;
+        } else {
+            return env('IMAGE_PATH') . $this->image;
+        }
     }
 
     public function getTranslation($locale = 'vi')

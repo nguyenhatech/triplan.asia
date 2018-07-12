ad7093f6343793a0070ac525d6f66d4987babeb1
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-12 15:01:49 +0700

    :zap: Service

--- resources/views/web/layouts/modules/header-scroll.blade.php
+++ resources/views/web/layouts/modules/header-scroll.blade.php
@@ -1,31 +1,34 @@
-<div class="header-scroll">
-    <div class="header-top-navigation">
-        <nav class="navbar navbar-expand-md navbar-light">
-            <a class="navbar-brand d-flex align-items-center" href="{{ env('APP_URL') }}">
-                <img id="logo_white" class="logo" src="{{ get_asset('web/images/logos/logo_blue.png') }}" alt="logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" id="td-header-search-button-mob" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Trang chủ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cẩm nang du lịch</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Tuyển dụng</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        <i class="far fa-user"></i>
-                        Đăng kí / Đăng nhập
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+<div class="fixed-top">
+    <div class="header-scroll">
+        <div class="header-top-navigation">
+            <nav class="navbar navbar-expand-md navbar-light">
+                <a class="navbar-brand d-flex align-items-center" href="{{ env('APP_URL') }}">
+                    <img id="logo_white" class="logo" src="{{ get_asset('web/images/logos/logo_blue.png') }}" alt="logo">
+                    <img id="logo_blue" style="display: none" class="logo" src="{{ get_asset('web/images/logos/logo_blue.png') }}" alt="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" id="td-header-search-button-mob" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Trang chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cẩm nang du lịch</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tuyển dụng</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                            <i class="far fa-user"></i>
+                            Đăng kí / Đăng nhập
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </div>
 </div>
--- resources/views/web/services/detail.blade.php
+++ resources/views/web/services/detail.blade.php
@@ -6,22 +6,46 @@
 
 @section('styles')
     <style type="text/css">
-        .detail-banner {
-            padding-top: 0px;
-            height: 500px;
+        .service-banner {
+            padding-top: 55px;
+            height: 300px;
             background-color: #ccc;
+            margin-bottom: 15px;
         }
-        .detail-banner img {
+        .service-banner img {
             object-fit: cover;
             width: 100%;
             height: 100%;
+        }
+        @media screen and (min-width: 768px) {
+            .service-banner {
+                height: 550px;
+            }
+        }
+
+        .service-info {
+
         }
     </style>
 @endsection
 
 @section('content')
-    <div class="detail-banner">
-        <img src="{{ $service->getImage() }}">
+    <div class="service-banner">
+        <img src="{{ $service->getImage() }}"  alt="{{ $service->getTranslation()->name }}" title="{{ $service->getTranslation()->name }}">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="service-info">
+                    <div class="name">
+                        <h1>{{ $service->getTranslation()->name }}</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                b
+            </div>
+        </div>
     </div>
 @endsection
 

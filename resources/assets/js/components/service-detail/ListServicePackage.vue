172ffa00e6130b14ccca3031ae9a7e3a1621424f
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-19 14:21:30 +0700

    Bookings

--- resources/assets/js/components/service-detail/ListServicePackage.vue
+++ resources/assets/js/components/service-detail/ListServicePackage.vue
@@ -1,22 +1,32 @@
 <template>
-    <div>
+    <div class="service-package">
         <div class="service-package__item">
-            <div class="d-flex justify-content-between">
-                <div>
+            <div class="package_parent d-flex justify-content-between">
+                <div class="name">
                     Gói Summer Discounted Ticket QR Code (tháng 7)
                 </div>
-                <div>
-                    <span>VND</span>
-                    <span>76.000 đồng</span>
-                    <span>Chọn</span>
+                <div class="d-flex flex-column">
+                    <div>
+                        <span class="currency">VND</span>
+                        <span class="price">76.000</span>
+                    </div>
+                    <span class="choose_package">Chọn</span>
                 </div>
             </div>
-            <div class="d-flex justify-content-between">
-                <div>
-                    Bên trái
-                </div>
-                <div>
-                    Bên phải
+            <div class="package_children">
+                <div class="package_children__item d-flex justify-content-between">
+                    <div>
+                        <span>
+                            Người lớn
+                        </span>
+                        <span>
+                            VND 1,444,009/Số lượng
+                        </span>
+                    </div>
+                    <div>
+                        <i class="fas fa-minus"></i>
+                        <i class="fas fa-plus"></i>
+                    </div>
                 </div>
             </div>
         </div>
@@ -65,5 +75,33 @@
 </script>
 
 <style type="text/css" scoped>
+.service-package {
+    padding: 10px 10px;
+    background-color: #f2f2f2;
+}
+.service-package__item {
+    padding: 10px 10px;
+    background-color: #fff;
 
+}
+.service-package__item .package_parent {
+    padding: 10px 0px 10px 15px;
+}
+.package_parent .name {
+    font-weight: bold;
+    font-size: 16px;
+}
+.package_parent .currency {
+    color: #777;
+    font-size: 12px;
+    font-weight: bold;
+}
+.package_parent .price {
+    color: #333;
+    font-size: 18px;
+    font-weight: bold;
+}
+.package_children__item {
+    padding-right: 50%;
+}
 </style>

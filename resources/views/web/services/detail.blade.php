6c366c00e9e799a17ec16325ca2f21816eb7e358
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-07-31 13:16:35 +0700

    Language

--- resources/views/web/services/detail.blade.php
+++ resources/views/web/services/detail.blade.php
@@ -388,15 +388,13 @@
 
             let height = Math.max( body.scrollHeight, body.offsetHeight,
                                    html.clientHeight, html.scrollHeight, html.offsetHeight );
-            console.log('1:', $(this).scrollTop());
-            console.log(height - 1200);
             if ($(this).scrollTop() > 500) {
                 $('.booking-service-destop').addClass('fixed');
             } else {
                 $('.booking-service-destop').removeClass('fixed');
             }
 
-            if ($(this).scrollTop() > (height - 1000)) {
+            if ($(this).scrollTop() > (height - 1280)) {
                 console.log('dưqdqwdq');
                 $('.booking-service-destop').removeClass('fixed');
             }

e423de7ef56499537edad34f2ad29caca0830ea9
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-08-25 11:27:26 +0700

    Email

--- resources/assets/js/components/booking/Thankyou.vue
+++ resources/assets/js/components/booking/Thankyou.vue
@@ -1,6 +1,183 @@
 <template>
     <div class="container">
-        <p>Thankyou</p>
+        <div style="background-color:#efefef">
+            <div style="margin:0;padding:0;background-color:#efefef;max-width: 600px; margin: auto">
+               <table align="center" cellpadding="0" cellspacing="0" style="padding:10px 0px 50px">
+                  <tbody>
+                     <tr>
+                        <td colspan="3" style="background-color:#fff;text-align:left;padding:13px 0px 12px 19px">
+                           <a href="https://triplan.asia" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://triplan.asia/&amp;source=gmail"><img src="https://triplan.asia/web/images/logos/logo_blue.png" alt="triplan.asia" width="140"></a>
+                        </td>
+                     </tr>
+                     <tr>
+                        <td colspan="6" style="padding:0px 0px 30px">
+                           <table align="center" cellpadding="0" cellspacing="0" width="100%">
+                              <tbody>
+                                 <tr>
+                                    <td colspan="2" style="background-color:#ebf4d0">
+                                       <table cellpadding="0" cellspacing="0" width="100%">
+                                          <tbody>
+                                             <tr>
+                                                <td colspan="2" align="center" style="padding:20px;font-weight:bold;font-size:24px;font-family:Arial;color:#91bb15;text-align:center;">
+                                                   XÁC NHẬN ĐẶT CỌC
+                                                </td>
+                                             </tr>
+                                          </tbody>
+                                       </table>
+                                    </td>
+                                 </tr>
+                                 <tr>
+                                    <td colspan="2" style="padding:27px 30px 10px;background-color:#fff;text-align:center">
+                                       <p style="font-size:14px;font-family:Arial;color:#000;margin-top:0px;text-align:center">
+                                          Chào , vui lòng thanh toán để hoàn thiện đặt tour tại Triplan của bạn.
+                                       </p>
+                                    </td>
+                                 </tr>
+                                 <tr>
+                                    <td colspan="2" style="padding:0px 20px 30px;background-color:#fff;text-align:center">
+                                       <table cellpadding="0" cellspacing="0" align="center" width="100%" style="background-color:#fffde7;border:1px dotted #f5a623;padding:12px 0px">
+                                          <tbody>
+                                             <tr>
+                                                <td style="font-size:14px;font-family:Arial;color:#000;text-align:center">Mã booking:
+                                                </td>
+                                             </tr>
+                                             <tr>
+                                                <td style="font-weight:bold;font-size:24px;font-family:Arial;color:#f8a000;text-align:center;text-transform: uppercase;"></td>
+                                             </tr>
+                                          </tbody>
+                                       </table>
+                                    </td>
+                                 </tr>
+                              </tbody>
+                           </table>
+                        </td>
+                     </tr>
+                     <tr>
+                        <td colspan="6" style="background-color:#fff;padding-bottom:30px">
+                           <table cellpadding="0" cellspacing="0" align="center" width="100%">
+                              <tbody>
+                                 <tr>
+                                    <td align="left" style="font-weight:bold;font-size:14px;font-family:Arial;color:#a9a9a9;padding:23px 0px 12px 30px">
+                                       Khởi hành
+                                    </td>
+                                    <td align="center" style="width:40px">&nbsp;</td>
+                                 </tr>
+                                 <tr>
+                                    <td align="left" style="padding-left:30px">
+                                       <p style="font-size:24px;font-family:Arial;color:#222;margin:0px;line-height:26px">
+
+                                       </p>
+                                    </td>
+                                 </tr>
+                              </tbody>
+                           </table>
+                        </td>
+                     </tr>
+                     <tr>
+                        <td height="30px" style="float:left">&nbsp;</td>
+                     </tr>
+                     <tr>
+                        <td colspan="6" style="background-color:#fff">
+                           <table cellpadding="0" cellspacing="0" width="100%" style="padding:0px 30px">
+                              <tbody>
+                                 <tr>
+                                    <td style="font-size:14px;font-family:Arial;padding:10px 0px;text-align:left;border-bottom:1px solid #ddd">
+                                       <p style="margin:2px 0px">
+                                            <strong>
+
+                                            </strong>
+                                       </p>
+                                    </td>
+                                    <td style="font-size:14px;font-family:Arial;color:#000;padding:10px 0px;text-align:right;border-bottom:1px solid #ddd">
+                                       <p style="margin:2px 0px"></p>
+                                    </td>
+                                 </tr>
+                                 <tr>
+                                    <td style="font-size:14px;font-family:Arial;color:#666666;padding:10px 0px;text-align:left;border-bottom:1px solid #ddd">
+                                       <p style="margin:2px 0px;color:#a0a0a0">Dịch vụ</p>
+                                    </td>
+                                    <td style="font-size:14px;font-family:Arial;color:#000;padding:10px 0px;text-align:right;border-bottom:1px solid #ddd">
+                                    </td>
+                                 </tr>
+                                 <tr>
+                                    <td style="font-size:14px;font-family:Arial;color:#666666;padding:10px 0px;text-align:left;border-bottom:1px solid #ddd">
+                                       <p style="margin:2px 0px;color:#a0a0a0">Khuyến mại</p>
+                                    </td>
+                                    <td style="font-size:14px;font-family:Arial;color:#000;padding:10px 0px;text-align:right;border-bottom:1px solid #ddd">
+                                    </td>
+                                 </tr>
+                                 <tr>
+                                    <td style="font-size:14px;font-family:Arial;color:#1b1b1b;padding:10px 0px;text-align:left;border-bottom:1px solid #ddd">
+                                       <strong>Tổng:</strong>
+                                    </td>
+                                    <td style="font-size:14px;font-family:Arial;color:#000;padding:10px 0px;text-align:right;border-bottom:1px solid #ddd">
+                                       <p style="margin:2px 0px">&nbsp;</p>
+                                       <p style="margin:2px 0px;font-size:24px;color:#91bb15"></p>
+                                    </td>
+                                 </tr>
+                                 <tr>
+                                    <td style="font-size:14px;font-family:Arial;color:#1b1b1b;padding:10px 0px;text-align:left;border-bottom:1px solid #ddd">
+                                       <strong>Thanh toán:</strong>
+                                       <p style="margin:2px 0px;color:#a0a0a0">Đã bao gồm thuế VAT và phí dịch vụ.</p>
+                                    </td>
+                                    <td style="font-size:14px;font-family:Arial;color:#000;padding:10px 0px;text-align:right;border-bottom:1px solid #ddd">
+                                       <p style="margin:2px 0px">&nbsp;</p>
+                                       <p style="margin:2px 0px;font-size:24px;color:#91bb15">
+
+                                       </p>
+                                    </td>
+                                 </tr>
+                                 <tr>
+                                    <td colspan="2" style="font-size:14px;font-family:Arial;color:#666;padding:10px 0px">Vui lòng
+                                       lưu ý: Ăn uống ngoài chương trình, vé thăm quan các điểm, hướng dẫn viên. Dịch vụ ngoài gói khuyến mãi
+                                    </td>
+                                 </tr>
+                              </tbody>
+                           </table>
+                        </td>
+                     </tr>
+                     <tr>
+                        <td colspan="6" height="10px" style="float:left">&nbsp;</td>
+                     </tr>
+                     <tr>
+                        <td colspan="6" height="30px" style="float:left">&nbsp;</td>
+                     </tr>
+                     <tr>
+                        <td colspan="6" style="background-color:#fff">
+                           <table cellpadding="0" cellspacing="0" width="100%" style="padding:0px 30px">
+                              <tbody>
+                                 <tr>
+                                    <td colspan="3" style="font-size:14px;font-family:Arial;padding:20px 0px 10px;text-align:left;border-bottom:1px solid #ddd">
+                                       <p style="font-weight:bold;color:#222;margin:0px">
+                                            Người nhận tour:
+                                       </p>
+                                    </td>
+                                 </tr>
+                                 <tr>
+                                    <td width="123px" style="font-size:14px;font-family:Arial;color:#222;padding:10px 10px 10px 0px;text-align:left;border-bottom:1px solid #ddd;vertical-align:top">
+                                       <p style="font-weight:bold;margin:0px;line-height:18px">Người nhận:
+                                       </p>
+                                    </td>
+                                    <td colspan="2" style="font-size:14px;font-family:Arial;color:#222;padding:10px 0px;border-bottom:1px solid #ddd">
+                                       <p style="margin:0px;line-height:25px"></p>
+                                       <p style="margin:0px;line-height:25px"></p>
+                                       <p style="margin:0px;line-height:25px"></p>
+                                    </td>
+                                 </tr>
+                              </tbody>
+                           </table>
+                        </td>
+                     </tr>
+                     <tr>
+                        <td colspan="6" height="10px" style="float:left">&nbsp;</td>
+                     </tr>
+                     <tr>
+
+                     </tr>
+                  </tbody>
+               </table>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,17 +190,11 @@
                 default: () => {
                     return {}
                 }
-            },
-            uuid: {
-                type: String,
-                default: () => {
-                    return '';
-                }
             }
         },
         data () {
             return {
-
+                booking: {}
             }
         },
         components: {
@@ -33,12 +204,22 @@
 
         },
         mounted () {
-
+            this.getBooking()
         },
         methods: {
             getBooking () {
-                if (!uuid) {
-                    console.log('huhuhuhuhuhuhuuh')
+                if (this.data_params.code) {
+                    console.log(this.data_params.code)
+                    axios.get('thankyou-booking/' + this.data_params.code).then(response => {
+                        console.log(response.data)
+                        switch (response.code) {
+                            case 200:
+                                this.booking = response.data
+                                break;
+                            default:
+                                break;
+                        }
+                    })
                 }
             },
         }

657a1b288e1c975d45e95f27d631fd9970b5232a
Kien NV <vankien9x@gmail.com>
2018-09-14 14:36:33 +0700

    Fix css

--- resources/assets/js/components/place/PlaceInfo.vue
+++ resources/assets/js/components/place/PlaceInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-12" style="margin-top: 20px;">
+        <div class="col-12 d-none d-lg-block" style="margin-top: 20px;">
             <swiper :options="swiperOption">
                 <swiper-slide v-for="(group, index) in serviceGroups" :key="index">
                     <div @click="toggleServiceGroupFilter(group.id)" :class="'service-group-box ' + (filters.service_group.length && !filters.service_group.includes(group.id) ? 'service-group-box-disabled' : '')" :style="{ backgroundImage: 'url(' + group.image_path + ')' }">
@@ -52,9 +52,9 @@
                                 <svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style="height: 24px; width: 24px; display: block; fill: rgb(118, 118, 118);"><path d="m10.4 18.2c-4.2-.6-7.2-4.5-6.6-8.8.6-4.2 4.5-7.2 8.8-6.6 4.2.6 7.2 4.5 6.6 8.8-.6 4.2-4.6 7.2-8.8 6.6m12.6 3.8-5-5c1.4-1.4 2.3-3.1 2.6-5.2.7-5.1-2.8-9.7-7.8-10.5-5-.7-9.7 2.8-10.5 7.9-.7 5.1 2.8 9.7 7.8 10.5 2.5.4 4.9-.3 6.7-1.7v.1l5 5c .3.3.8.3 1.1 0s .4-.8.1-1.1" fill-rule="evenodd"></path></svg>
                             </button>
                         </div>
-                        <input type="text" value="" id="input-search" class="form-control typeahead" :placeholder="(trans ? trans.web_home_baner_top_placehoder_input : 'Tìm dịch vụ ở ') + place_name" aria-describedby="button-addon2" style="border-right: 0px;" v-model="filters.q">
-                        <div class="input-group-append">
-                            <button v-if="filters.q" @click="filters.q = ''" class="btn" type="button" id="button-addon2">
+                        <input type="text" value="" id="input-search" :class="'form-control typeahead' + (filters.q ? ' has-search-key' : '')" :placeholder="(trans ? trans.web_home_baner_top_placehoder_input : 'Tìm dịch vụ ở ') + place_name" aria-describedby="button-addon2" v-model="filters.q">
+                        <div v-if="filters.q" class="input-group-append">
+                            <button @click="filters.q = ''" class="btn" type="button" id="button-addon2">
                                 <svg viewBox="0 0 24 24" role="img" focusable="false" style="height: 12px; width: 12px; display: block; fill: rgb(118, 118, 118);"><path d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22" fill-rule="evenodd"></path></svg>
                             </button>
                         </div>
@@ -435,6 +435,9 @@
     }
     .custom-control-input:checked~.custom-control-label::before {
         background-color: #02a676;
+    }
+    .has-search-key {
+        border-right: 0px;
     }
     @keyframes fadeInUp {
         from {

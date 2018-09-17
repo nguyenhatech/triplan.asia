9a983eeb1a7c4c165220fe269f78521d14f91b49
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-09-17 16:43:37 +0700

    Booking

--- resources/assets/js/components/service-detail/ListServicePackage.vue
+++ resources/assets/js/components/service-detail/ListServicePackage.vue
@@ -146,7 +146,7 @@
                 _.forEach(_this.data_booking.service_info.service_package_parent_actives.data, (item) => {
                     if (item.checked) {
                         _.forEach(item.service_package_children_actives.data , (item2) => {
-                            if (item.quantity) {
+                            if (item2.quantity) {
                                 this.setArrayServicePackages(item2)
                             }
                         })

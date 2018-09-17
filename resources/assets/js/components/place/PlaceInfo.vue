662a07b8c954592c9e315ec4128794904fc9a4b4
Kien NV <vankien9x@gmail.com>
2018-09-17 15:33:55 +0700

    Fix show place

--- resources/assets/js/components/place/PlaceInfo.vue
+++ resources/assets/js/components/place/PlaceInfo.vue
@@ -347,14 +347,14 @@
                 forEach(this.serviceTypeList, type => {
                   if (this.filters.service_group.includes(type.service_group_id)) {
                       this.serviceTypes.push(type)
-                      this.fetchServices()
-                      this.updateUrlParams()
                   } else {
                     if (this.filters.service_type.indexOf(type.id) !== -1) {
                         this.filters.service_type.splice(this.filters.service_type.indexOf(type.id))
                     }
                   }
                 })
+                this.fetchServices()
+                this.updateUrlParams()
             }, 500),
             'filters.service_type': debounce(function () {
                 this.fetchServices()
--- resources/assets/js/mixins/index.js
+++ resources/assets/js/mixins/index.js
@@ -4,12 +4,16 @@ export const helper = {
     },
     methods: {
         trimText (text, number) {
-          var html = new DOMParser().parseFromString(text, 'text/html')
-          var plainText = html.body.textContent || ''
-          if (plainText.length < number) {
-            return plainText
+          if (text) {
+            var html = new DOMParser().parseFromString(text, 'text/html')
+            var plainText = html.body.textContent || ''
+            if (plainText.length < number) {
+              return plainText
+            } else {
+              return plainText.substr(0, number) + '...'
+            }
           } else {
-            return plainText.substr(0, number) + '...'
+            return ''
           }
         }
     }

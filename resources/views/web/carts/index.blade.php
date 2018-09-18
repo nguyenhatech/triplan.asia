f6fd10fe101424c82dd04ad014f68443b21b2d5d
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-09-18 16:47:53 +0700

    Cart

--- resources/views/web/carts/index.blade.php
+++ resources/views/web/carts/index.blade.php
@@ -19,7 +19,8 @@
             color: #ff424e;
             font-size: 18px;
         }
-        .data-cart-right-checkout .btn-checkout {
+        .data-cart-right-checkout .btn-checkout,
+        .data-cart-empty .btn-checkout {
             color: #fff;
             background-color: #ff424e;
             border-color: #ff424e;
@@ -28,7 +29,7 @@
         }
         .data-cart {
             background-color: #fff;
-            min-height: 20px;
+            border-radius: 4px;
         }
         .data-cart img {
             width: 180px;
@@ -56,6 +57,14 @@
             color: #ff424e;
             cursor: pointer;
         }
+        .data-cart-empty {
+            background-color: #fff;
+            min-height: 300px;
+            border-radius: 4px;
+        }
+        .data-cart-empty .btn-checkout {
+            padding: 8px 45px;
+        }
     </style>
 @endsection
 
@@ -67,73 +76,89 @@
                     <h6 class="text-uppercase font-weight-bold">Giỏ hàng</h6>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-9">
-                    <div class="data-cart">
-                        <table class="table table-hover">
-                            <tbody>
-                                @forelse ($dataCart as $item)
-                                    <tr>
-                                        <td width="180">
-                                            <img src="{{ $item->service_info->thumb_path }}" alt="{{ $item->service_info->name }}">
-                                        </td>
-                                        <td width="250">
-                                            <div class="info d-flex flex-column">
-                                                <span class="name">{{ $item->service_info->name }}</span>
-                                                <span class="package_name">Gói: {{ $item->package_name }}</span>
-                                                <span class="date">Ngày: {{ $item->date }}</span>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            Chi tiết tiền
-                                        </td>
-                                        <td width="120">
-                                            <div class="action">
-                                                <a class="item" href="#">
-                                                    <i class="far fa-edit"></i>
-                                                    Sửa
-                                                </a>
-                                                <span class="item delete btn-delete-cart"
-                                                    data-url="{{ route('web.cart.delete-one-cart', $item->service_info->id) }}"
-                                                    data-name="{{ $item->service_info->name }}">
-                                                    <i class="far fa-trash-alt"></i>
-                                                    Xóa
-                                                </span>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @empty
+            @if (count($dataCart))
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="data-cart">
+                            <table class="table table-hover">
+                                <tbody>
+                                    @forelse ($dataCart as $item)
+                                        <tr>
+                                            <td width="180">
+                                                <img src="{{ $item->service_info->thumb_path }}" alt="{{ $item->service_info->name }}">
+                                            </td>
+                                            <td width="250">
+                                                <div class="info d-flex flex-column">
+                                                    <span class="name">{{ $item->service_info->name }}</span>
+                                                    <span class="package_name">Gói: {{ $item->package_name }}</span>
+                                                    <span class="date">Ngày: {{ $item->date }}</span>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                Chi tiết tiền
+                                            </td>
+                                            <td width="120">
+                                                <div class="action">
+                                                    <a class="item" href="#">
+                                                        <i class="far fa-edit"></i>
+                                                        Sửa
+                                                    </a>
+                                                    <span class="item delete btn-delete-cart"
+                                                        data-url="{{ route('web.cart.delete-one-cart', $item->service_info->id) }}"
+                                                        data-name="{{ $item->service_info->name }}">
+                                                        <i class="far fa-trash-alt"></i>
+                                                        Xóa
+                                                    </span>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @empty
 
-                                @endforelse
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="data-cart-right">
-                        <div class="provisional">
-                            <span class="d-flex justify-content-between">
-                                <span class="font-weight-light">Tạm tính:</span>
-                                <span>13.500.400 đ</span>
-                            </span>
+                                    @endforelse
+                                </tbody>
+                            </table>
                         </div>
-                        <div class="provisional">
-                            <span class="d-flex justify-content-between">
-                                <span class="font-weight-light">Thành tiền:</span>
-                                <span class="d-flex flex-column align-items-end">
-                                    <span class="font-weight-bold price">13.500.400 đ</span>
-                                    <span class="font-weight-light" style="font-size: 12px;">(Đã bao gồm VAT)</span>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="data-cart-right">
+                            <div class="provisional">
+                                <span class="d-flex justify-content-between">
+                                    <span class="font-weight-light">Tạm tính:</span>
+                                    <span>13.500.400 đ</span>
                                 </span>
-                            </span>
+                            </div>
+                            <div class="provisional">
+                                <span class="d-flex justify-content-between">
+                                    <span class="font-weight-light">Thành tiền:</span>
+                                    <span class="d-flex flex-column align-items-end">
+                                        <span class="font-weight-bold price">13.500.400 đ</span>
+                                        <span class="font-weight-light" style="font-size: 12px;">(Đã bao gồm VAT)</span>
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="pt-3 data-cart-right-checkout">
+                            <a href="#" class="btn btn-large btn-block btn-danger btn-checkout">
+                                Tiến hành đặt hàng
+                            </a>
                         </div>
                     </div>
-                    <div class="pt-3 data-cart-right-checkout">
-                        <a href="#" class="btn btn-large btn-block btn-danger btn-checkout">
-                            Tiến hành đặt hàng
-                        </a>
+                </div>
+            @else
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="data-cart data-cart-empty d-flex justify-content-center align-items-center">
+                            <div class="d-flex flex-column align-items-center justify-content-center">
+                                <img class="mb-3" src="{{ get_asset('web/images/download/cartEmpty.png')}}">
+                                <h5 class="pb-3">Không có sản phẩm nào trong giỏ hàng</h5>
+                                <a href="{{ route('web.home') }}" class="btn btn-danger btn-checkout">
+                                    Trở về trang chủ
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 @endsection

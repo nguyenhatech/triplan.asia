84e330442ad3663cabe3dc309048ad061d4711c2
Ngo Nguyen Thuc <nnthuc2402@gmail.com>
2018-10-05 16:46:39 +0700

    Big update

--- resources/views/web/become-merchant/index.blade.php
+++ resources/views/web/become-merchant/index.blade.php
@@ -4,14 +4,19 @@
     <style type="text/css">
         .become-host {
             padding-top: 80px;
-            min-height: 1000px;
+            height: 100vh;
             background-color: #f7f7f7;
+            background-image: url('{{ get_asset('web/images/login/0ffd8594-f123-43f0-85bb-7ef88c6f0624.jpg') }}');
+            background-size: cover;
+            background-position: center;
         }
     </style>
 @endsection
 
 @section('content')
-    <div class="become-host"></div>
+    <div class="become-host">
+
+    </div>
 @endsection
 
 @section('scripts')
